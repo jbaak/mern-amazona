3918d2f7d92ae3cfec30a5433da8c5a960e657ac
Jesus Baak <jesusbaak@hotmail.com>
2022-11-01 14:50:34 -0600

    Se agrega seccion de listado de productos y detalle de productos, se agregan estilos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,24 @@
 
+import { BrowserRouter, Link, Route, Routes} from 'react-router-dom'
+import HomeScreen from "./screeens/HomeScreen";
+import ProductScreen from './screeens/ProductScreen';
+
 
 function App() {
   return (
-    <div >
-      <header >
-        <a href="/">Amazona</a>
-      </header>
-      <main>
-        list products
-      </main>
-    </div>
+    <BrowserRouter>
+      <div >
+        <header >
+          <Link to="/">Amazona</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<HomeScreen />}></Route>
+            <Route path="/product/:slug" element={<ProductScreen />}/>
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
